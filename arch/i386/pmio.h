9dd3c35418a18bc488132a7a5a31c47bd9a0eeb8
dtxc <69575053+dtxc@users.noreply.github.com>
2024-08-03 18:16:58 +0300

    Add macros for certain hardware ports & values
    Add port macros for keyboard, PIT (dat and cmd), PIC master and slave ports and end of interrupt

--- arch/i386/pmio.h
+++ arch/i386/pmio.h
@@ -4,6 +4,26 @@
 #include <stdint.h>
 #include <string.h>
 
+
+#define PIC_MASTER_CMD 0x20
+#define PIC_MASTER_DAT 0x21
+#define PIC_SLAVE_CMD  0xA0
+#define PIC_SLAVE_DAT  0xA1
+
+#define PIC_EIO 0x20
+
+#define VGA_CMD 0x3D4
+#define VGA_DAT 0x3D5
+
+#define KEYBOARD_DAT 0x60
+#define KEYBOARD_STATUS 0x64
+
+#define PIT_DAT0 0x40
+#define PIT_DAT1 0x41
+#define PIT_DAT2 0x42
+#define PIT_CMD  0x43
+
+
 inline void outb(uint16_t port, uint8_t val)
 {
     asm volatile ("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
